29a0df2e00ceb696f1bc13640ceeafae03b5a29e
Ilshin Ji <ilshin714@gmail.com>
2022-10-10 09:09:19 -0400

    Made OperationContract and implemented it to the service cs

---
+++ WcfCalculationService/WcfCalculationService/Calculation.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfCalculationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Calculation" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Calculation.svc or Calculation.svc.cs at the Solution Explorer and start debugging.
+    public class Calculation : ICalculation
+    {
+
+        public bool IsPrimeNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintHTMLString(string tag, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReverseString(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] SortFiveNumbers(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SumDigits(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WcfCalculationService/WcfCalculationService/ICalculation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfCalculationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculation" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculation
+    {
+        // 1) Check if the int parater is prime numer. 
+        [OperationContract]
+        bool IsPrimeNumber(int number);
+
+        // 2) Print sum of all the digits of the int parameter
+        [OperationContract]
+        int SumDigits(int number);
+
+        // 3) Reverse the string paramter and return the result
+        [OperationContract]
+        string ReverseString(string word);
+
+        // 4) Print HTML tags to the string paramter and print the tag string
+        [OperationContract]
+        void PrintHTMLString(string tag, string text);
+
+        // 5) get sort type and an array of 5 numbers,
+        // sort 5 numbers accordin to the sort type and return the array
+        [OperationContract]
+        int[] SortFiveNumbers(int[] numbers);
+    }
+}
